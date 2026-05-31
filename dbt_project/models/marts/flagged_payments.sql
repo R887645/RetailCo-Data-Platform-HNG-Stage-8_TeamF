8143fb6cf39b2810b11314be2e74ed7c5878c9b2
Rofiah Abdul-Azeez <kehindeazeez49@gmail.com>
2026-05-31 18:38:47 +0100

    Update flagged_payments.sql

--- dbt_project/models/marts/flagged_payments.sql
+++ dbt_project/models/marts/flagged_payments.sql
@@ -4,34 +4,41 @@
 -- Payments with amount_paid = 0 or unexplained negative amounts
 -- are stored here for investigation by the compliance team.
 -- This is NOT a fact table and does not appear in the bus matrix.
--- It does not need conformed dimensions.
 
 with payments as (
-    select * from {{ ref('stg_payments') }}
+    select * from {{ ref('stg_payments') }}
+),
+
+dim_customer as (
+    select * from {{ ref('dim_customer') }}
+    where is_current = true
 ),
 
 final as (
-    select
-        {{ dbt_utils.generate_surrogate_key(
-            ['payment_id']
-        ) }}                                as flagged_payment_sk,
-        payment_id,
-        order_id,
-        customer_id,
-        amount_paid,
-        currency,
-        case
-            when amount_paid = 0
-                then 'zero amount payment'
-            when amount_paid < 0
-                and payment_type != 'refund'
-                then 'unexplained negative amount'
-            else 'other anomaly'
-        end                                 as flag_reason,
-        created_at                          as flagged_at
-    from payments
-    where amount_paid = 0
-       or (amount_paid < 0 and payment_type != 'refund')
+    select
+        {{ dbt_utils.generate_surrogate_key(
+            ['p.payment_id']
+        ) }}                                as flagged_payment_sk,
+        p.payment_id,
+        p.order_id,
+        dc.customer_sk,
+        p.amount_paid,
+        p.currency,
+        p.payment_type,
+        case
+            when p.amount_paid = 0
+                then 'zero amount payment'
+            when p.amount_paid < 0
+                and p.payment_type != 'refund'
+                then 'unexplained negative amount'
+            else 'other anomaly'
+        end                                 as reason,
+        p.created_at                        as flagged_at
+    from payments p
+    left join dim_customer dc
+        on dc.customer_id = p.customer_id
+    where p.amount_paid = 0
+       or (p.amount_paid < 0 and p.payment_type != 'refund')
 )
 
 select * from final
