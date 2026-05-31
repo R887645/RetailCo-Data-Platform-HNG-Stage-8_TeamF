58f3744c84845cb9e284e29467b3697678dd9032
Rofiah Abdul-Azeez <kehindeazeez49@gmail.com>
2026-05-31 21:05:30 +0100

    Update dim_store.sql

--- dbt_project/models/marts/dimensions/dim_store.sql
+++ dbt_project/models/marts/dimensions/dim_store.sql
@@ -1,17 +1,25 @@
 -- dim_store.sql
--- No SCD2 needed: store attributes are stable
-with source as (
+-- Business justification: store dimension with surrogate key.
+-- Stores represent physical retail locations across Nigeria.
+-- No SCD2 needed: store changes are not tracked historically.
+-- Surrogate key store_sk used in all fact tables instead of
+-- natural key store_id to follow Kimball principles.
+
+with stores as (
     select * from {{ ref('stg_stores') }}
 ),
+
 final as (
     select
-        {{ dbt_utils.generate_surrogate_key(['store_id']) }}    as store_sk,
+        {{ dbt_utils.generate_surrogate_key(['store_id']) }}
+                                   as store_sk,
         store_id,
         store_name,
         city,
         state,
         address,
         manager_name
-    from source
+    from stores
 )
+
 select * from final
